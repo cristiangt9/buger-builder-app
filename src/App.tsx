19979bc1d86caf3edddb97e8c61fbcbff518f93d
Cristian Gonzalez <cristiangt9@gmail.com>
2021-04-28 16:02:35 -0500

    delete files innecessaries

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Redirect, Route, Switch } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./components/layout/Layout";
 import Builder from "./pages/Builder";
 import Checkout from "./pages/Checkout";
 
